b8ea5462e2e0479efa45a04f8a830cd114f8cc3b
kaku-kun <t.suezaki@icloud.com>
2019-11-18 02:35:32 +0900

    Create functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+import React  from 'react';
 
 const Bar =()=> (<label htmlFor="bar">bar</label>);
 
-class App extends Component {
-  render () {
-
-    const greeting = 'Hi';
-    return (
-      <React.Fragment>
-        <Bar />
-        <input type="text" onChange={()=>console.log('Hello Mi')}/>
-        <p>{greeting}</p>
-      </React.Fragment>
-
-    )
-  }
+const App = () => {
+  return (
+    <React.Fragment>
+      <Bar />
+      <input type="text" onChange={()=>console.log('Say Hello')} />
+    </React.Fragment>
+  )
 };
+
+
+// class App extends Component {
+//   render () {
+
+//     const greeting = 'Hi';
+//     return (
+//       <React.Fragment>
+//         <Bar />
+//         <input type="text" onChange={()=>console.log('Hello Mi')}/>
+//         <p>{greeting}</p>
+//       </React.Fragment>
+
+//     )
+//   }
+// };
 
 export default App;
 
