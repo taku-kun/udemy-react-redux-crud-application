12b634622b3bc83947b6c258dc57ee6e7439cb73
kaku-kun <t.suezaki@icloud.com>
2019-11-18 01:58:47 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
 
+const Bar =()=> (<label htmlFor="bar">bar</label>);
+
 class App extends Component {
   render () {
-    return <h1>Hello World</h1>;
+
+    const greeting = 'Hi';
+    return (
+      <React.Fragment>
+        <Bar />
+        <input type="text" onChange={()=>console.log('Hello Mi')}/>
+        <p>{greeting}</p>
+      </React.Fragment>
+
+    )
   }
 };
 
 export default App;
+
+// return <input type="text" onClick={()=> console.log('Hello World')} />
+    // const greeting = 'Nice!'
+    // const dom = <h1 className='foo'>{greeting}</h1>;
+    // return dom;
+
+  //   <div>
+  //   <Bar />
+  //   <input type="text" onChange={()=>console.log('Hello Mi')}/>
+  //   <p>{greeting}</p>
+  // </div>
