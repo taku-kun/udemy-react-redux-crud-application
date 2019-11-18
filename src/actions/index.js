5808e6d1eaa49d5063125f30c5ec5574ef4970ca
kaku-kun <t.suezaki@icloud.com>
2019-11-18 10:35:00 +0900

    create action creater

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+// action creater
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const increment = () => ({type: 'INCREMENT'});
+export const decrement = () => ({type: 'DECREMENT'});
+
